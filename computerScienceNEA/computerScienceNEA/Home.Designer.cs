1f58e088e813fee3361fc912302413507b461ef2
nomaba <118121397+nomaba@users.noreply.github.com>
2026-01-03 14:41:13 +0000

    started home page

--- computerScienceNEA/computerScienceNEA/Home.Designer.cs
+++ computerScienceNEA/computerScienceNEA/Home.Designer.cs
@@ -30,23 +30,60 @@ namespace computerScienceNEA
         private void InitializeComponent()
         {
             this.labelHelloTag = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonLogOut = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // labelHelloTag
             // 
             this.labelHelloTag.AutoSize = true;
-            this.labelHelloTag.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHelloTag.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelHelloTag.Location = new System.Drawing.Point(12, 9);
             this.labelHelloTag.Name = "labelHelloTag";
-            this.labelHelloTag.Size = new System.Drawing.Size(184, 37);
+            this.labelHelloTag.Size = new System.Drawing.Size(116, 24);
             this.labelHelloTag.TabIndex = 0;
             this.labelHelloTag.Text = "Hello Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(191, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "What do you want to do first?";
+            // 
+            // buttonLogOut
+            // 
+            this.buttonLogOut.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogOut.Location = new System.Drawing.Point(12, 36);
+            this.buttonLogOut.Name = "buttonLogOut";
+            this.buttonLogOut.Size = new System.Drawing.Size(158, 37);
+            this.buttonLogOut.TabIndex = 2;
+            this.buttonLogOut.Text = "Save data and log out";
+            this.buttonLogOut.UseVisualStyleBackColor = true;
+            this.buttonLogOut.Click += new System.EventHandler(this.buttonLogOut_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(321, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Play a game";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonLogOut);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.labelHelloTag);
             this.Name = "Home";
             this.Text = "Home";
@@ -59,5 +96,8 @@ namespace computerScienceNEA
         #endregion
 
         private System.Windows.Forms.Label labelHelloTag;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonLogOut;
+        private System.Windows.Forms.Button button1;
     }
 }
--- computerScienceNEA/computerScienceNEA/Home.cs
+++ computerScienceNEA/computerScienceNEA/Home.cs
@@ -19,7 +19,14 @@ namespace computerScienceNEA
 
         private void Home_Load(object sender, EventArgs e)
         {
+            // get user's first name and display it on labelHelloTag //////////////////////////////////////////////////////////////////////////////////////////////////
 
+            // get user's favourite colour and then make that the background colour ///////////////////////////////////////////////////////////////////////////////////
+        }
+
+        private void buttonLogOut_Click(object sender, EventArgs e)
+        {
+            // send SQL commands to save the user data into the database and then close the application////////////////////////////////////////////////////////////////
         }
     }
 }
